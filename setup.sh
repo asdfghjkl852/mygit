777f69409cee572d0748209a3f34f07e3d98499d
asdfghjkl852 <Phiraphon123456789@gmail.com>
2023-04-08 13:34:17 +0700

    .

---
+++ setup.sh
@@ -0,0 +1,22 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"Phiraphon123456789@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
+setup.sh
+
+from sklearn.neighbors import KNeighborsClassifier
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
